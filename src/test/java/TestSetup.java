a81994bdaa6670dd749a3126e695ee381ca2530d
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-07 14:47:35 +0530

    Initial commit

---
+++ src/test/java/TestSetup.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestSetup {
+
+    @Test
+    void testSetup() {
+        Assertions.assertEquals(1, 1);
+    }
+}
